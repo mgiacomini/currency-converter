44044f4f44913876410bde01c3bca978728ab676
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2022-05-17 15:42:33 -0300

    adding currency conversor behaviour

--- lib/currency_conversor.ex
+++ lib/currency_conversor.ex
@@ -1,9 +1,31 @@
 defmodule CurrencyConversor do
   @moduledoc """
-  CurrencyConversor keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
+  This module defines an adapter interface and exposes a public API to convert amounts of money into another currencies.
   """
+
+  @typedoc "Currency type using international notation. Ex: `BRL` for Brazilian Real"
+  @type currency() :: String.t()
+
+  @doc """
+  Converts an amount from one currency to another one.
+
+  ## Examples
+
+    To convert USD 10 into EUR:
+
+    iex> convert("USD", "EUR", Decimal.from_float(10.0))
+    {:ok, %Decimal{...}}
+
+  """
+  @callback convert(currency(), currency(), Decimal.t()) :: {:ok, Decimal.t()} | {:error, any()}
+
+  @doc "Calls an external service to get a real-time currency conversion"
+  @spec convert(currency(), currency(), Decimal.t()) :: {:ok, Decimal.t()} | {:error, any()}
+  def convert(from, to, amount) do
+    impl!().convert(from, to, amount)
+  end
+
+  defp impl! do
+    Application.get_env(:currency_conversor, :adapter) || raise "No adapter was configured."
+  end
 end
---
+++ test/currency_conversor_test.exs
@@ -0,0 +1,26 @@
+defmodule CurrencyConversorTest do
+  use ExUnit.Case
+  import Mox
+
+  setup :verify_on_exit!
+
+  @amount Decimal.from_float(100.0)
+  @from "BRL"
+  @to "USD"
+
+  describe "convert/3" do
+    test "raises an error if no implementation was configured" do
+      Application.put_env(:currency_conversor, :adapter, nil)
+
+      assert_raise RuntimeError, "No adapter was configured.", fn ->
+        CurrencyConversor.convert("", "", %Decimal{})
+      end
+    end
+
+    test "returns the adapter response" do
+      Application.put_env(:currency_conversor, :adapter, CurrencyConversorMock)
+      expect(CurrencyConversorMock, :convert, fn @from, @to, @amount -> {:ok, %Decimal{}} end)
+      assert {:ok, %Decimal{}} == CurrencyConversor.convert(@from, @to, @amount)
+    end
+  end
+end
