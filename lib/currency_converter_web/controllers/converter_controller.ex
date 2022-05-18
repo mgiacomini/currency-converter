b938e7eaa60a1f60352838924c325472a6f1b49e
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2022-05-18 09:59:57 -0300

    Extract validation and casting to another func

--- lib/currency_converter_web/controllers/converter_controller.ex
+++ lib/currency_converter_web/controllers/converter_controller.ex
@@ -1,26 +1,24 @@
 defmodule CurrencyConverterWeb.ConverterController do
   use CurrencyConverterWeb, :controller
 
-  @spec convert(Plug.Conn.t(), any) :: Plug.Conn.t()
-  def convert(conn, %{"amount" => amount}) when not is_float(amount) do
-    conn
-    |> put_status(:unprocessable_entity)
-    |> render("conversion_error.json", reason: "amount must be a float")
-  end
-
-  def convert(conn, params) do
-    case CurrencyConverter.convert(
-           params["from"],
-           params["to"],
-           Decimal.from_float(params["amount"])
-         ) do
-      {:ok, amount} ->
-        render(conn, "conversion.json", amount: amount)
-
+  @spec convert(Plug.Conn.t(), map) :: Plug.Conn.t()
+  def convert(conn, %{} = attrs) do
+    with {:ok, attrs} <- validate_and_cast_attrs(attrs),
+         {:ok, amount} <- CurrencyConverter.convert(attrs["from"], attrs["to"], attrs["amount"]) do
+      render(conn, "conversion.json", amount: amount)
+    else
       {:error, reason} ->
         conn
         |> put_status(:unprocessable_entity)
         |> render("conversion_error.json", reason: reason)
     end
+  end
+
+  defp validate_and_cast_attrs(%{} = attrs) do
+    amount = attrs["amount"]
+
+    if is_float(amount),
+      do: {:ok, Map.put(attrs, "amount", Decimal.from_float(amount))},
+      else: {:error, "amount must be a float"}
   end
 end
